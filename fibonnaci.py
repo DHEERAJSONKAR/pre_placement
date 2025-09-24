bdb0bdcde93fc2b0131f72d31b71343fd25931d3
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-09-24 23:43:13 +0530

    Refactor Fibonacci implementation to include a separate function for generating the Fibonacci series

--- fibonnaci.py
+++ fibonnaci.py
@@ -6,6 +6,11 @@ def fibonacci(n, fib={}):
     result = fibonacci(n-1, fib)+ fibonacci(n-2,fib)
     fib[n]= result
     return result
-
+def fibonnaci_series(n):
+    fib_series = []
+    for i in range(n+1):
+        fib_series.append(fibonacci(i))
+    return fib_series
 n= int(input("enter the number: "))
 print(f"the fibonnaci series is : {fibonacci(n)}")
+print(f"The Fibonacci series up to {n} is: {fibonnaci_series(n)}")
